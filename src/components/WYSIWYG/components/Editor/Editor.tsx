16a28639f86ee9e83083468ff8bd49d15de88f3d
Mohab Elbasiry <mohabahmedo15@gmail.com>
2025-02-27 16:57:51 +0200

    setup github actions

--- src/components/WYSIWYG/components/Editor/Editor.tsx
+++ src/components/WYSIWYG/components/Editor/Editor.tsx
@@ -14,7 +14,6 @@ const DraftEditor = ({
     if (alignment) {
       return `align-${alignment}`;
     }
-
     return "";
   }, []);
   return (
--- src/components/WYSIWYG/types/toolbarOptions.type.ts
+++ src/components/WYSIWYG/types/toolbarOptions.type.ts
@@ -1,17 +1,93 @@
- 
-export type ToolbarOption= {
-  bold?: { enabled: boolean; customRender?: (props: { action: () => void; label: string }) => React.JSX.Element };
-  italic?: { enabled: boolean; customRender?: (props: { action: () => void; label: string }) => React.JSX.Element };
-  underline?: { enabled: boolean; customRender?: (props: { action: () => void; label: string }) => React.JSX.Element };
-  strikethrough?: { enabled: boolean; customRender?: (props: { action: () => void; label: string }) => React.JSX.Element };
-  blockquote?: { enabled: boolean; customRender?: (props: { action: () => void; label: string }) => React.JSX.Element };
-  codeBlock?: { enabled: boolean; customRender?: (props: { action: () => void; label: string }) => React.JSX.Element };
-  unorderedList?: { enabled: boolean; customRender?: (props: { action: () => void; label: string }) => React.JSX.Element };
-  orderedList?: { enabled: boolean; customRender?: (props: { action: () => void; label: string }) => React.JSX.Element };
-  alignLeft?: { enabled: boolean; customRender?: (props: { action: () => void; label: string }) => React.JSX.Element };
-  alignCenter?: { enabled: boolean; customRender?: (props: { action: () => void; label: string }) => React.JSX.Element };
-  alignRight?: { enabled: boolean; customRender?: (props: { action: () => void; label: string }) => React.JSX.Element };
-  redo?: { enabled: boolean; customRender?: (props: { action: () => void; label: string }) => React.JSX.Element };
-  undo?: { enabled: boolean; customRender?: (props: { action: () => void; label: string }) => React.JSX.Element };
-   
-}
+export type ToolbarOption = {
+  bold?: {
+    enabled: boolean;
+    customRender?: (props: {
+      action: () => void;
+      label: string;
+    }) => React.JSX.Element;
+  };
+  italic?: {
+    enabled: boolean;
+    customRender?: (props: {
+      action: () => void;
+      label: string;
+    }) => React.JSX.Element;
+  };
+  underline?: {
+    enabled: boolean;
+    customRender?: (props: {
+      action: () => void;
+      label: string;
+    }) => React.JSX.Element;
+  };
+  strikethrough?: {
+    enabled: boolean;
+    customRender?: (props: {
+      action: () => void;
+      label: string;
+    }) => React.JSX.Element;
+  };
+  blockquote?: {
+    enabled: boolean;
+    customRender?: (props: {
+      action: () => void;
+      label: string;
+    }) => React.JSX.Element;
+  };
+  codeBlock?: {
+    enabled: boolean;
+    customRender?: (props: {
+      action: () => void;
+      label: string;
+    }) => React.JSX.Element;
+  };
+  unorderedList?: {
+    enabled: boolean;
+    customRender?: (props: {
+      action: () => void;
+      label: string;
+    }) => React.JSX.Element;
+  };
+  orderedList?: {
+    enabled: boolean;
+    customRender?: (props: {
+      action: () => void;
+      label: string;
+    }) => React.JSX.Element;
+  };
+  alignLeft?: {
+    enabled: boolean;
+    customRender?: (props: {
+      action: () => void;
+      label: string;
+    }) => React.JSX.Element;
+  };
+  alignCenter?: {
+    enabled: boolean;
+    customRender?: (props: {
+      action: () => void;
+      label: string;
+    }) => React.JSX.Element;
+  };
+  alignRight?: {
+    enabled: boolean;
+    customRender?: (props: {
+      action: () => void;
+      label: string;
+    }) => React.JSX.Element;
+  };
+  redo?: {
+    enabled: boolean;
+    customRender?: (props: {
+      action: () => void;
+      label: string;
+    }) => React.JSX.Element;
+  };
+  undo?: {
+    enabled: boolean;
+    customRender?: (props: {
+      action: () => void;
+      label: string;
+    }) => React.JSX.Element;
+  };
+};
